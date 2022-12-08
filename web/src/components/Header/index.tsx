93928c97f910cf84ffcce659ece466b3d4a8165f
João Guilherme Da Rocha <joaoguibc@gmail.com>
2022-12-08 17:18:41 +0000

    Add a dropdown menu to header

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,10 +1,22 @@
-import Image from "next/image"
 import Link from "next/link"
+import Image from "next/image"
 import { ComponentProps } from "react"
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
+import { BookBookmark, CaretRight, Gear, IdentificationBadge, SignOut, UsersFour } from "phosphor-react"
 
 import { Avatar } from "../Avatar"
+import { Separator } from "../Separator"
 
-import { Container, UserInfoContainer } from "./styles"
+import { theme } from "../../../stitches.config"
+import {
+  Container,
+  DropdownMenuContent,
+  DropdownMenuSubContent,
+  UserInfoContainer,
+  DropdownMenuArrow,
+  DropdownMenuItem,
+  DropdownMenuSubTrigger
+} from "./styles"
 
 interface HeaderProps extends ComponentProps<typeof Container> { }
 
@@ -15,13 +27,70 @@ export function Header(props: HeaderProps) {
         <Image src="/logo_egepe.png" alt="Logo EGEPE" quality={100} width={156} height={88} priority />
       </Link>
 
-      <Link href="/login">
+      {/*<Link href="/login">
         <UserInfoContainer>
           <span>Olá, <br /> Faça login</span>
 
           <Avatar />
         </UserInfoContainer>
-      </Link>
+        </Link>*/}
+
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger asChild>
+          <UserInfoContainer>
+            <span>Olá, <br /> João Guilherme Da Rocha</span>
+
+            <Avatar src="https://github.com/JoaoGuiBC.png" />
+          </UserInfoContainer>
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Portal>
+          <DropdownMenuContent align="end" sideOffset={8}>
+            <DropdownMenuArrow />
+
+            <DropdownMenuItem>
+              <IdentificationBadge size={24} color={theme.colors.gray900.value} weight="light" />
+              <span>Perfil</span>
+            </DropdownMenuItem>
+
+            <Separator />
+            <DropdownMenuItem>
+              <BookBookmark size={24} color={theme.colors.gray900.value} weight="light" />
+              <span>Meus cursos</span>
+            </DropdownMenuItem>
+
+            <Separator />
+            <DropdownMenu.Sub>
+              <DropdownMenuSubTrigger>
+                  <Gear size={24} color={theme.colors.gray900.value} weight="light" />
+                  <span>Administrar plataforma</span>
+                  <CaretRight size={16} color={theme.colors.gray900.value} weight="light" />
+              </DropdownMenuSubTrigger>
+
+              <DropdownMenu.Portal>
+                <DropdownMenuSubContent sideOffset={16}>
+                  <DropdownMenuItem>
+                    <UsersFour size={24} color={theme.colors.gray900.value} weight="light" />
+                    <span>Usuários</span>
+                  </DropdownMenuItem>
+
+                  <DropdownMenuItem>
+                    <BookBookmark size={24} color={theme.colors.gray900.value} weight="light" />
+                    <span>Cursos</span>
+                  </DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenu.Portal>
+            </DropdownMenu.Sub>
+
+            <Separator />
+            <DropdownMenuItem>
+              <SignOut size={24} color={theme.colors.gray900.value} weight="light" />
+              <span>Sair</span>
+            </DropdownMenuItem>
+
+          </DropdownMenuContent>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
     </Container>
   )
 }
--- web/src/components/Header/styles.ts
+++ web/src/components/Header/styles.ts
@@ -1,4 +1,6 @@
-import { styled } from "../../../stitches.config";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
+
+import { styled } from "../../../stitches.config"
 
 export const Container = styled('header', {
   display: 'flex',
@@ -17,6 +19,7 @@ export const UserInfoContainer = styled('div', {
   display: 'flex',
   alignItems: 'center',
   gap: '0.5rem',
+  cursor: 'pointer',
 
   '& span': {
     fontFamily: '$text',
@@ -25,5 +28,84 @@ export const UserInfoContainer = styled('div', {
     textAlign: 'right',
 
     color: '$gray100',
+  }
+})
+
+export const DropdownMenuContent = styled(DropdownMenu.Content, {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  padding: '0.5rem',
+  gap: '0.5rem',
+  isolation: 'isolate',
+  alignSelf: 'flex-end',
+  position: 'relative',
+
+  background: '$white',
+
+  border: '1px solid $gray300',
+  filter: 'drop-shadow(0 8px 8px rgba(0, 0, 0, 0.15))',
+  borderRadius: '8px'
+})
+
+export const DropdownMenuSubContent = styled(DropdownMenu.SubContent, {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  padding: '0.5rem',
+  gap: '0.5rem',
+  isolation: 'isolate',
+  alignSelf: 'flex-end',
+  position: 'relative',
+
+  background: '$white',
+
+  border: '1px solid $gray300',
+  filter: 'drop-shadow(0 8px 8px rgba(0, 0, 0, 0.15))',
+  borderRadius: '8px'
+})
+
+export const DropdownMenuArrow = styled(DropdownMenu.Arrow, {
+  position: 'absolute',
+  right: '96px',
+
+  fill: '$white'
+})
+
+export const DropdownMenuItem = styled(DropdownMenu.Item, {
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  gap: '0.25rem',
+
+  width: '100%',
+  cursor: 'pointer',
+  userSelect: 'none',
+
+  '& span': {
+    fontFamily: '$text',
+    fontWeight: '$regular',
+    fontSize: '$md',
+    textAlign: 'left',
+    color: '$gray900'
+  }
+})
+
+export const DropdownMenuSubTrigger = styled(DropdownMenu.SubTrigger, {
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  gap: '0.25rem',
+
+  width: '100%',
+  cursor: 'pointer',
+  userSelect: 'none',
+
+  '& span': {
+    fontFamily: '$text',
+    fontWeight: '$regular',
+    fontSize: '$md',
+    textAlign: 'left',
+    color: '$gray900'
   }
 })
