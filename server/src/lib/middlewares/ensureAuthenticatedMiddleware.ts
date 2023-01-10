7650d98151e441c87664f1bd7b848b35990d5bff
João Guilherme Da Rocha <joaoguibc@gmail.com>
2023-01-10 15:30:50 +0000

    Make ensureAuthenticatedMiddleware set a renewed jwt into Headers

--- server/src/lib/middlewares/ensureAuthenticatedMiddleware.ts
+++ server/src/lib/middlewares/ensureAuthenticatedMiddleware.ts
@@ -1,9 +1,10 @@
 import { Middleware } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { verify } from "https://deno.land/x/djwt@v2.8/mod.ts";
 
-import { errorMiddleware } from "./errorMiddleware.ts";
 import { key } from "../keys/jwtKey.ts";
 import { AppError } from "../appError.ts";
+import { errorMiddleware } from "./errorMiddleware.ts";
+import { renewUserSession } from "../../services/sessions/renewUserSession.ts";
 
 export const ensureAuthenticatedMiddleware: Middleware = async (ctx, next) => {
   const jwt = await ctx.cookies.get("token");
@@ -14,6 +15,10 @@ export const ensureAuthenticatedMiddleware: Middleware = async (ctx, next) => {
     if (!result) {
       throw new AppError("Usuário não autenticado", 401);
     }
+
+    const { token } = await renewUserSession(String(result.sub));
+
+    ctx.response.headers.set("jwt", token);
 
     next();
   } else {
