1cf6eb0355f49909411c1fdaf51de0d52687d348
João Guilherme Da Rocha <joaoguibc@gmail.com>
2023-03-03 18:00:58 +0000

    Make login functional

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,8 +1,8 @@
-import NextAuth from 'next-auth'
-import EmailProvider from 'next-auth/providers/email'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-import { prisma } from '@lib/prisma'
+import { prisma } from "@lib/prisma";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -13,6 +13,7 @@ export default NextAuth({
     }),
   ],
   pages: {
-    signIn: '/login',
+    signIn: "/login",
+    verifyRequest: "/login?message=verifyEmail",
   },
-})
+});
--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,19 +1,21 @@
-import { z } from 'zod'
-import { NextSeo } from 'next-seo'
-import { signIn } from 'next-auth/react'
-import { EnvelopeSimple } from 'phosphor-react'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from "zod";
+import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+import { useForm } from "react-hook-form";
+import { EnvelopeSimple } from "phosphor-react";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { Text, Button, Heading, TextInput } from '@components/index'
+import { Text, Button, Heading, TextInput } from "@components/index";
 
-import { ButtonContainer, Container, Form, InputContainer } from './styles'
-import { useForm } from 'react-hook-form'
+import { ButtonContainer, Container, Form, InputContainer } from "./styles";
+import { useEffect } from "react";
 
 const informEmailFormSchema = z.object({
-  email: z.string().email({ message: 'Por favor, informe um e-mail válido.' }),
-})
+  email: z.string().email({ message: "Por favor, informe um e-mail válido." }),
+});
 
-type InformEmailFormData = z.infer<typeof informEmailFormSchema>
+type InformEmailFormData = z.infer<typeof informEmailFormSchema>;
 
 export default function Login() {
   const {
@@ -22,46 +24,59 @@ export default function Login() {
     formState: { errors, isSubmitting },
   } = useForm<InformEmailFormData>({
     resolver: zodResolver(informEmailFormSchema),
-  })
+  });
+
+  const router = useRouter();
+  const { message } = router.query;
 
   async function handleLogin({ email }: InformEmailFormData) {
-    signIn('email', { email })
+    console.log(isSubmitting);
+    await signIn("email", { email });
   }
 
   return (
     <>
       <NextSeo
         title="Login | PLACUR"
-        description="Informe suas credenciais para entrar na plataforma."
+        description="Informe seue e-mail para entrar na plataforma."
       />
 
       <Container>
         <Heading style="secondary">Estamos quase lá.</Heading>
 
-        <Text>
-          Informe o seu e-mail, caso não tenha uma conta ela será criada
-          automaticamente.
-        </Text>
+        {message === "verifyEmail" ? (
+          <Text size="lg">
+            Por favor, confira o seu e-mail, enviamos a sua confirmação de login
+            por lá.
+          </Text>
+        ) : (
+          <>
+            <Text>
+              Informe o seu e-mail, caso não tenha uma conta ela será criada
+              automaticamente.
+            </Text>
 
-        <Form onSubmit={handleSubmit(handleLogin)}>
-          <InputContainer>
-            <TextInput
-              Icon={EnvelopeSimple}
-              type="email"
-              placeholder={
-                errors.email ? `E-mail - ${errors.email.message}` : 'E-mail'
-              }
-              {...register('email')}
-            />
-          </InputContainer>
+            <Form onSubmit={handleSubmit(handleLogin)}>
+              <InputContainer>
+                <TextInput
+                  Icon={EnvelopeSimple}
+                  type="email"
+                  placeholder={
+                    errors.email ? `E-mail - ${errors.email.message}` : "E-mail"
+                  }
+                  {...register("email")}
+                />
+              </InputContainer>
 
-          <ButtonContainer>
-            <Button type="submit" disabled={isSubmitting}>
-              Login
-            </Button>
-          </ButtonContainer>
-        </Form>
+              <ButtonContainer>
+                <Button type="submit" disabled={isSubmitting}>
+                  Login
+                </Button>
+              </ButtonContainer>
+            </Form>
+          </>
+        )}
       </Container>
     </>
-  )
+  );
 }
